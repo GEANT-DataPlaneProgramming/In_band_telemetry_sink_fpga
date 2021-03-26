d5ea8fe1e3baddc347c07ecab394c4fe2b516349
Patova <xpatov00@stud.fit.vutbr.cz>
2021-03-26 09:12:45 +0000

    P4INT - merge multyheader version with current new version

--- sink_fpga/p4int_influxdb/p4int.cc
+++ sink_fpga/p4int_influxdb/p4int.cc
@@ -2,10 +2,9 @@
  * @author Mario Kuka <kuka@cesnet.cz>
  *         Pavlina Patova <xpatov00@stud.fit.vutbr.cz>
  * @brief INT sink node
- *     
- * Copyright (c) 2015 - 2018 CESNET, z.s.p.o.
  */
 
+#include <iostream>
 #include <cstdio>
 #include <getopt.h>
 #include <errno.h>
@@ -16,9 +15,23 @@
 #include <ctime>
 #include <cmath>
 #include <map>
-
+#include <fstream>
+#include <inttypes.h>
+    
 #include "device.h"
 #include "p4int.h"
+#include "p4_influxdb.h"
+
+/**
+ * Packet counter
+ */
+uint64_t pkt_cnt = 0;
+
+/**
+ * Packet drop counter
+ */
+uint64_t pkt_drop = 0;
+
 
 /**
  * Helping control variable
@@ -34,30 +47,34 @@ static std::map<uint64_t, meta_data> flow_map;
  * Setup the stop flag 
  */
 void setup_stop(int sig) {
+    printf("\ntotal - %lu\ndrop - %lu\n", pkt_cnt, pkt_drop);
     stop = 1;
 }
 
 /**
  * Sleep in microseconds
+ * \param us Number of microseconds
  */
-static void delay_usecs(unsigned int us)
-{
+void delay_usecs(unsigned int us) {
     struct timespec t1;
     struct timespec t2;
 
-    if (us == 0)
+    if (us == 0) {
         return;
+    }
 
     t1.tv_sec = (us / 1000000);
     t1.tv_nsec = (us % 1000000) * 1000;
 
     // NB: Other variants of sleep block whole process.
 retry:
-    if (nanosleep((const struct timespec *)&t1, &t2) == -1)
+    if (nanosleep((const struct timespec *)&t1, &t2) == -1) {
         if (errno == EINTR) {
             t1 = t2; 
             goto retry;
         }
+    }
+
     return;
 }
 
@@ -97,6 +114,7 @@ uint64_t ntoh64(uint64_t value) {
 
 /**
  * Print telemetric data
+ * \param hdr Structureof telemetric data
  */
 void print_telemetric(const telemetric_hdr_t *hdr) {
     printf("Orig TS       => %lu\n", hdr->origTs);
@@ -115,110 +133,147 @@ void print_telemetric(const telemetric_hdr_t *hdr) {
  * \param pkt Input packet to prs
  * \param influxdb Database descriptor
  * \param opt Program parameters
- * \return RET_OK iff everything was fine.
+ * \return RET_OK if everything was fine
  */
-uint32_t process_packet(struct ndp_packet& pkt, std::unique_ptr<influxdb::InfluxDB> &influxdb, const options_t& opt) {
-/* ~~~ Packet structure ~~~
- *
- * srcAddr          32b
- * dstAddr          32b
- * node_cnt         8b
- * --- FOR EACH NODE ---
- * ingress_port_id  16b
- * egress_port_id   16b
- * ingress_tstamp   64b
- * egress_tstamp    64b
- * ndk_tstamp       64b
- * delay            64b
- */
-// Prepare telemetric data  into the apropriate structure
+uint32_t process_packet(struct ndp_packet& pkt, IntExporter &exporter, const options_t& opt) {  
+    /* ~~~ Packet structure ~~~
+    *
+    * srcAddr          32b
+    * dstAddr          32b
+    * node_cnt         8b
+    * --- FOR EACH NODE ---
+    * ingress_port_id  16b
+    * egress_port_id   16b
+    * ingress_tstamp   64b
+    * egress_tstamp    64b
+    * ndk_tstamp       64b
+    * delay            64b
+    */
+    // Prepare telemetric data into the apropriate structure
     telemetric_hdr_t tmpHdr;
-    // uint16_t hdrLength = 4 + 4 + 1;
     uint16_t nodeLength = 2 + 2 + 8 + 8 + 8 + 8; 
-    
-    // Convert IP addresses 
+
+     // Convert IP addresses 
     inet_ntop(AF_INET, pkt.data, tmpHdr.srcIp, IP_BUFF_SIZE);
     inet_ntop(AF_INET, pkt.data + 4, tmpHdr.dstIp, IP_BUFF_SIZE);
-    
-    uint8_t node_cnt = (*((uint8_t*) (pkt.data + 8)));
-    
+
+    uint8_t node_cnt = (*((uint8_t*) (pkt.data + 8)));   
     printf("Node cnt      => %hu\n", node_cnt);
 
     // FOR EACH NODE
     for(uint16_t i = 0; i < node_cnt; i++){
 
-    // Convert source and destination ports
-    tmpHdr.srcPort =  ntohs(*((uint16_t*) (pkt.data + 9 + nodeLength*i)));
-    tmpHdr.dstPort =  ntohs(*((uint16_t*) (pkt.data + 11 + nodeLength*i)));
-    
-    // Convert source timestamp
-    tmpHdr.origTs = ntoh64(*((uint64_t*) (pkt.data+13 + nodeLength*i)));
-    tmpHdr.dstTs =  ntoh64(*((uint64_t*) (pkt.data+21  + nodeLength*i)));
+        // Convert source and destination ports
+        tmpHdr.srcPort =  ntohs(*((uint16_t*) (pkt.data + 9 + nodeLength*i)));
+        tmpHdr.dstPort =  ntohs(*((uint16_t*) (pkt.data + 11 + nodeLength*i)));
 
-    // Convert destination timestamp
-    //   tmpHdr.dstTs = ntohl((*(uint32_t*)(pkt.data + 33 + nodeLength*i))) ;
-    //   tmpHdr.dstTs += ntohl((*((uint32_t*)(pkt.data + 29 + nodeLength*i)))) *  1000000000ll; 
-    // Cut of timestamps to 48 bits
-    if(opt.tstmp == 1) {
-        uint64_t mask = 0x0000FFFFFFFFFFFF;
-        tmpHdr.origTs = tmpHdr.origTs & mask;
-        tmpHdr.dstTs = tmpHdr.dstTs & mask;
-    }
-  
-
-/*
-// Get flow data
-    uint64_t map_key = *((uint64_t*)(pkt.data));
-    meta_data &meta_tmp = flow_map[map_key];
-   
-// Calculate int header
-    tmpHdr.delay = tmpHdr.dstTs - tmpHdr.origTs;
-    tmpHdr.seqNum = meta_tmp.seq; 
-    tmpHdr.sink_jitter = tmpHdr.dstTs - meta_tmp.prev_dstTs;
-    tmpHdr.reordering = 0;   
-    
-// Update flow data
-    meta_tmp.prev_dstTs = tmpHdr.dstTs;
-    meta_tmp.seq++;
-*/
-// Report to influxdb
-/*    if(opt.hostValid) {
-        uint32_t ret = p4_influxdb_send_packet(influxdb,tmpHdr, &opt);
-        if(ret != RET_OK) {
-            printf("Error during the export to InfluxDB\n");
-            return RET_ERR;
+        // Convert source timestamp
+        tmpHdr.origTs = ntoh64(*((uint64_t*) (pkt.data + 13 + nodeLength*i)));
+        
+        // Convert destination timestamp
+        tmpHdr.dstTs = ntohl((*(uint32_t*)(pkt.data + 33 + nodeLength*i)));
+        tmpHdr.dstTs += ntohl((*((uint32_t*)(pkt.data + 28 + nodeLength*i)))) *  1000000000ll; 
+        
+        // Cut of timestamps to 48 bits
+        if(opt.tstmp == 1) {
+            uint64_t mask = 0x0000FFFFFFFFFFFF;
+            tmpHdr.origTs = tmpHdr.origTs & mask;
+            tmpHdr.dstTs = tmpHdr.dstTs & mask;
         }
-    }  
-  */    
-    // Print to console
-    if(opt.verbose) {
-        print_telemetric(&tmpHdr);
+
+        // Get flow data
+        // TODO: Implement flow hash table 
+        uint64_t map_key = *((uint64_t*)(pkt.data));
+        meta_data &meta_tmp = flow_map[map_key];
+    
+        // Calculate int header
+        tmpHdr.delay = tmpHdr.dstTs - tmpHdr.origTs;
+        tmpHdr.seqNum = ntohl((*(uint32_t*)(pkt.data + 44 + nodeLength*i))); 
+        tmpHdr.sink_jitter = tmpHdr.dstTs - meta_tmp.prev_dstTs;
+        
+        if(meta_tmp.seq == 0) {
+            tmpHdr.reordering = 0; 
+        } else {
+            tmpHdr.reordering = tmpHdr.seqNum - meta_tmp.seq - 1; 
+        } 
+        
+        // Update flow data
+        meta_tmp.prev_dstTs = tmpHdr.dstTs;
+        meta_tmp.seq = tmpHdr.seqNum;
+
+        // Report to influxdb
+        if(opt.hostValid && (pkt_cnt % opt.smpl_rate == 0)) {
+            uint32_t ret = exporter.sendData(tmpHdr);
+            if(ret != EXIT_SUCCESS) {
+                //printf("Error during the export to InfluxDB\n");
+                //return RET_ERR;
+                pkt_drop++;
+                if(pkt_drop % 1000 == 0 && pkt_drop != 0) {
+                    std::cout << "dopped: " << pkt_drop << std::endl;
+                }
+            }
+        }  
+        
+        // Print to console
+        if(opt.verbose) {
+            print_telemetric(&tmpHdr);
+        }
     }
-}
     return RET_OK;
 }
 
 /** 
  * Print the help
+ * \param prgname Name of program 
  */
-void print_help(const char* prgname){
-    printf("%s [-d device] [-c collectorAddress] [-p collectorPort] [-r collectorProtocol] [-u username] [-s password] [-b numOfReports] [-l logFile] [-vth]\n", prgname);
-    printf("\t* -d = ID of the device (e.g.,0 stands for /dev/nfb0, default is 0).");
-    printf("\t* -c = Host address of the collector.");
+void print_help(const char* prgname) {
+    printf("%s [-d device] [-c collectorAddress] [-p collectorPort] [-r collectorProtocol]" 
+           " [-u username] [-s password] [-b numOfReports] [-l logFile] [-m samplingRate]"
+           " [-i buffer_size] [-vtkh]\n", prgname);
+    printf("\t* -d = ID of the device (e.g.,0 stands for /dev/nfb0, default is 0).\n");
+    printf("\t* -c = Host address of the collector.\n");
     printf("\t* -p = Port of collector.\n");
     printf("\t* -r = Protocol of collector.\n");
     printf("\t* -u = Username of collector.\n");
     printf("\t* -s = Password of collector.\n");
     printf("\t* -b = How many reports send at once (default is 1000).\n"); 
     printf("\t* -l = Error messages will be written to given log file.\n"); 
+    printf("\t* -m = Set sampling rate of reporting to database (default is 1).\n"); 
+    printf("\t* -i = Number of senders.\n"); 
     printf("\t* -v = Enable the verbose mode for printinf of parsed data.\n"); 
-    printf("\t* -t = Enable 48-bit timestamp mode\n");
-    printf("\t* -k = Configure P4 device\n");
-    printf("\t* -h = Prints the help\n");
+    printf("\t* -t = Enable 48-bit timestamp mode.\n");
+    printf("\t* -k = Disable P4 device configuration.\n");
+    printf("\t* -h = Prints the help.\n");
 }
 
 /**
- * Parse passed arguments and prepare the configuration
+ * Load rules for flow filter
+ * \param file Load from this file
+ * \param opt Program parameters
+ */
+void load_flt(const char *file, options_t *opt) {
+    std::ifstream infile(file);
+    if (infile.fail()) {
+        fprintf(stderr, "Failed to open file \"%s\"\n", file);
+        exit(1);
+    }
+    
+    std::string line;
+    while (std::getline(infile, line)) {
+        std::array<uint8_t, 6> array;
+        uint16_t port;
+        
+        sscanf(line.c_str(), "%" SCNu8 ".%" SCNu8 ".%" SCNu8 ".%" SCNu8 " %" SCNu16, 
+            &array[3], &array[2], &array[1], &array[0], &port);
+        array[4] = port & 0xff;
+        array[5] = (port >> 8) & 0xff;
+
+        opt->ip_flt.push_back(array);
+    }
+}   
+
+/**
+ * Parse arguments and prepare the configuration
  *
  * \param opt Poiter to the \ref options_t strcture
  * \param argc Number of passed arguments
@@ -233,15 +288,15 @@ static int32_t parse_arguments(options_t* opt, int32_t argc, char** argv) {
     opt->log = 0;
     opt->verbose = 0;
     opt->tstmp = 0;   
-    opt->p4cfg = 0;   
+    opt->p4cfg = 1;
+    opt->smpl_rate = 1;
+    opt->raw_buffer = 1; 
 
-    // tmp for parsing parameters
     int32_t op;
     char* tmp;
-    opterr = 0;
-    
+     
     // Parse all parameters
-    while((op = getopt(argc, argv, "d:c:p:r:u:s:b:l:vtkh")) != -1) {
+    while((op = getopt(argc, argv, "d:c:p:r:u:s:b:l:m:f:i:vtkh")) != -1) {
         switch(op) {
             case 'd':
                 // Parse the device ID
@@ -290,8 +345,22 @@ static int32_t parse_arguments(options_t* opt, int32_t argc, char** argv) {
             case 'l':
                 // Log file
                 opt->log = 1;
-                //opt->logFile = fopen(optarg,"a");
                 strcpy(opt->logFile, optarg);
+                break;
+            
+            case 'm':
+                // Size of batch
+                opt->smpl_rate = atoi(optarg);
+                break;
+            
+            case 'f':
+                // Load flow filter
+                load_flt(optarg, opt);
+                break;
+            
+            case 'i':
+                // Size of internal raw buffer
+                opt->raw_buffer = atoi(optarg);
                 break;
             
             case 'v':
@@ -306,7 +375,7 @@ static int32_t parse_arguments(options_t* opt, int32_t argc, char** argv) {
             
             case 'k':
                // Configure P4 device
-               opt->p4cfg = 1;
+               opt->p4cfg = 0;
                break;
             
             case 'h':
@@ -322,32 +391,70 @@ static int32_t parse_arguments(options_t* opt, int32_t argc, char** argv) {
     return RET_OK;
 }
 
+/**
+ * Processing all incoming packets
+ * \param device P4 device
+ * \param nfb NFB device
+ * \param opt Program parameters
+ * \return RET_OK if everything was fine
+ */
+int loop_proccess(p4device_t &device, nfb_int_dev_t &nfb, IntExporter &exporter, options_t &opt) {
+    uint32_t pkt_rx_ret;
+    uint32_t ret_pkt_proc;
+    struct ndp_packet packets[NDP_PACKET_BUFF]; 
+    
+    while(!stop) {
+        // Read the packet from the buffer
+        pkt_rx_ret = ndp_rx_burst_get(nfb.rx, packets, NDP_PACKET_BUFF);
+    
+        // flush influxdb buffer
+        if(pkt_rx_ret == 0) {
+            delay_usecs(100);
+            continue;
+        }
+
+        // Process all packets 
+        for(uint8_t i = 0; i < pkt_rx_ret; i++) {
+            // Increment counter for each finished one 
+            pkt_cnt++;
+            // Process packet
+            ret_pkt_proc = process_packet(packets[i], exporter, opt);
+            if(ret_pkt_proc != RET_OK) {
+                printf("Error during the packet processing!\n");
+                close_device(&device, &opt, &nfb);
+                return RET_ERR;
+            }
+        }
+
+        // Mark all read packets as finished   
+        if(pkt_rx_ret != 0) {
+            ndp_rx_burst_put(nfb.rx); 
+        }
+    }
+   
+    close_device(&device,&opt,&nfb);
+    return RET_OK;
+}
+
+
 int32_t main(int32_t argc, char** argv) {
     // Prepare the configuration
     int32_t ret;
     options_t opt;
-    ret=parse_arguments(&opt, argc, argv);
-    if(ret != RET_OK) return RET_ERR;
-    
-    // Prepare influxdb
-    std::unique_ptr<influxdb::InfluxDB> influxdb;
-/*    if(opt.hostValid) {
-        ret = p4_influxdb_open_socket(influxdb, &opt);
-        if(ret != RET_OK) {
-            printf("Unable to connect to the Inxlux DB\n");
-            return RET_ERR;
-        }
+    if(parse_arguments(&opt, argc, argv) != RET_OK) {
+        return RET_ERR;
     }
-  */  
+    
     // Prepare device 
     p4device_t device;
     nfb_int_dev_t nfb;
     ret = open_device(&device, &opt, &nfb);
     if(ret != RET_OK) {
-        /* Close all already opened parts */
-        close_device(&device,&opt,&nfb);
+        // Close all already opened parts
+        close_device(&device, &opt, &nfb);
         return RET_ERR;
     }
+
     // Configure the device
     if(opt.p4cfg) {
         ret = configure_device(&device,&opt);
@@ -363,49 +470,12 @@ int32_t main(int32_t argc, char** argv) {
         close_device(&device, &opt, &nfb);
         return RET_ERR;
     }
-
-    // Loop until the CTRL+C is caught
-    uint32_t pkt_rx_ret;
-    uint32_t ret_pkt_proc;
-    struct ndp_packet packets[NDP_PACKET_BUFF]; 
-    uint32_t empty_buff = 0;
-    while(!stop) {
-        // Read the packet from the buffer
-        pkt_rx_ret = ndp_rx_burst_get(nfb.rx, packets, NDP_PACKET_BUFF);
+  
+    // Prepare exporter 
+    IntExporter exporter(&opt);
     
-        // flush influxdb buffer
-        if(pkt_rx_ret == 0) {
-            empty_buff++;
-            if(empty_buff == 100) {
-                try {   
-                    //influxdb->flushBuffer();
-                }
-                catch(...) {
-                }
-            }
-            delay_usecs(100);
-            continue;
-        }
-        else {
-            empty_buff = 0;
-        }
-
-        // Process all read packets - increment counter for each finished one
-        for(uint8_t i = 0; i < pkt_rx_ret; i++) {
-            ret_pkt_proc = process_packet(packets[i], influxdb, opt);
-            if(ret_pkt_proc != RET_OK) {
-               printf("Error during the packet processing!\n");
-               close_device(&device, &opt, &nfb);
-               return  RET_ERR;
-            }
-        }
-
-        // Mark all read packets as finished   
-        if(pkt_rx_ret != 0) {
-            ndp_rx_burst_put(nfb.rx); 
-        }
-    }
-   
-    close_device(&device,&opt,&nfb);
-    return RET_OK;
+    // infinite loop packet processing
+    ret = loop_proccess(device, nfb, exporter, opt);
+    
+    return ret;
 }
