a8faac0da98b1ffa959b6566b4ff4bbbcc9675df
Mário Kuka <kuka@cesnet.cz>
2021-04-04 09:14:35 +0000

    Update P4 firmware for INT standard 1.0

--- sink_fpga/p4_src/generate_p4.py
+++ sink_fpga/p4_src/generate_p4.py
@@ -2,14 +2,10 @@ from jinja2 import Template
 import shutil
 import sys
 
-int_meta_file = open("./temps/int_meta_hdr.txt", "r")
-meta_tm = Template(int_meta_file.read())
-meta_msg = meta_tm.render(n=int(sys.argv[1]))
-
 parser_file = open("./temps/parser.txt", "r")
 parser_tm = Template(parser_file.read())
 parser_file = open("./parser.p4", "w")
-parser_file.write(parser_tm.render(meta=meta_msg))
+parser_file.write(parser_tm.render(n=int(sys.argv[1])))
 
 shutil.copy("./temps/header.txt", "./header.p4")
 shutil.copy("./temps/top.txt", "./top.p4")
